33905f1ac08ba4b36ca408a874ebb96c0f9f6428
abs365 <blue2gtv@gmail.com>
2026-06-01 07:28:22 +0100

    chore: add playwright-report and test-results to .gitignore
    Generated test output should not be tracked in version control.
    Removes existing tracked playwright-report/ and test-results/ directories.

--- test-results/.playwright-artifacts-0/traces/resources/9bcc2252e53e3658c8444e5b88161febed8fca94.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/lib_supabase_client_ts_0pv6zon._.js","static/chunks/_0xvmxok._.js","static/chunks/node_modules_0z~gvha._.js"],
-    source: "dynamic"
-});
--- test-results/.playwright-artifacts-0/traces/resources/e39e6ca5e01436aa47502ff83f121c320f2ecd48.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/node_modules_0_hfe35._.js","static/chunks/app_error_tsx_00~l3vx._.js"],
-    source: "dynamic"
-});
